60176bf85afd0a7d0a50b2374bcb95ac72fdef92
Praveen Shah <pshah@supplyvalid.com>
2025-09-11 15:54:57 +0530

    updated part

--- practice_work/01_variable.js
+++ practice_work/01_variable.js
@@ -3,6 +3,20 @@ let accountEmail="praveen@google.com"
 var accountPasssword ="12345"
 accountCity ="Jaipur"
 
-// accountId = 2 // not allowed
-console.log(accountID);
+
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity ="Bengaluru"
+
+console.log (accountId);
+/*
+prefer not to use var
+because of issue in block scope and funcational scope 
+
+*/
+
+
+console.table([accountId , accountEmail, accountPassword , accountCity])
+
+
 
