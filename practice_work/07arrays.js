ac2a9895d9e1e6c47c8d76877a60b85e0ac96345
Praveen Shah <pshah@supplyvalid.com>
2025-09-12 13:20:47 +0530

    arrrys in js

---
+++ practice_work/07arrays.js
@@ -0,0 +1,23 @@
+const myArr = [0,1,2,3,4,5,]
+const myHeros =["shaktiman", "hero2" , "hero3"]
+
+
+const myArr2 =new Array(1,2,3,4)
+console.log(myArr[2]);
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+myArr.unshift(9)
+myArr.shift()
+
+//console.log(myArr>includes(9));
+//console.log(myArr.indexzOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(typeof newArr);
