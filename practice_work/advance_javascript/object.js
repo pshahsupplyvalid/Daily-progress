42f520933ca10d132defccf5c6efed4b44bc899f
Praveen Shah <pshah@supplyvalid.com>
2025-09-22 12:18:26 +0530

    about objects

---
+++ practice_work/advance_javascript/object.js
@@ -0,0 +1,29 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+console.log(descripter);
+
+//console.log(Math.PI);
+//Math.PI =5
+//console.log(Math.PI);
+
+
+const chai =
+{
+    name: 'ginger',
+    price : 250,
+    isAvailable : true
+
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+// Object.defineProperties(chai,'name' ,{
+//     writable: false,
+//     enumerable: false
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+for (let[key,value] of Object.entries(chai)){
+    console.log(`${key}: ${value}`); 
+}
